15de5223a0c8483f3893e5aa5bda8f42c5f88c2e
Andrii <lutsiukandrey1@gmail.com>
2023-08-10 14:07:52 +0300

    add form in modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,22 @@
 "use client";
 
 import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
   return (
     <div className="p-4">
-      <p>dssdfgg</p>
-      <Modal title="test" description="hrllo" isOpen onClose={() => {}}>
-        Children
-      </Modal>
+      <p>Root page</p>
     </div>
   );
 };
