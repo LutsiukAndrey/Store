f9eb781fbd8714fc066451235f1fcd8c514d1ccf
Andrii <lutsiukandrey1@gmail.com>
2023-08-12 14:40:08 +0300

    update NavBar

--- app/(dashboard)/[soreId]/(routes)/page.tsx
+++ app/(dashboard)/[soreId]/(routes)/page.tsx
@@ -1,16 +1,17 @@
 import prismadb from "@/lib/prismadb";
 
 interface DashboardProps {
-  params: { storeId: string };
+  params: { soreId: string };
 }
 
 const DashboardPage: React.FC<DashboardProps> = async ({ params }) => {
+  console.log(params);
   const store = await prismadb.store.findFirst({
     where: {
-      id: params.storeId,
+      id: params.soreId,
     },
   });
-
+  // console.log(store);
   return <div>Active Store: {store?.name}</div>;
 };
 
