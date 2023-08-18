2e1d51e6d3eae0349bc7db4e55e0ca36b7d1a893
Andrii <lutsiukandrey1@gmail.com>
2023-08-18 15:20:08 +0300

    test commit

--- app/(auth)/(routes)/layout.tsx
+++ app/(auth)/(routes)/layout.tsx
@@ -3,6 +3,7 @@ export default function AuthLoyout({
 }: {
   children: React.ReactNode;
 }) {
+  console.log("");
   return (
     <div className="flex items-center justify-center h-full ">{children}</div>
   );
